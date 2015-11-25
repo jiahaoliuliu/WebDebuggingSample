baa172813faaafe1b54af20904f4c56b656fbba1
Jiahao Liu <jiahao.liu@dubizzle.com>
2015-11-25 15:17:04 +0400

    Created simple web view for dubizzle

--- app/src/main/java/com/jiahaoliuliu/webdebuggingsample/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/webdebuggingsample/MainActivity.java
@@ -2,12 +2,26 @@ package com.jiahaoliuliu.webdebuggingsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String WEB_URL = "https://dubai.dubizzle.com/";
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Link the views
+        mWebView = (WebView) findViewById(R.id.web_view);
+        mWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl(WEB_URL);
     }
 }
